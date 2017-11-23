55ab591412ee59f8412905b235ac5dd0802b29a7
JuanFerrer <juanemilioferrer@outlook.com>
2017-11-23 11:56:22 +0000

    Add BTMLParser for C++

--- C#/BTMLParser/Program.cs
+++ C#/BTMLParser/Program.cs
@@ -9,88 +9,95 @@ namespace BTMLParser
 {
     class Program
     {
-        static char[] separatorArray = {' '};
+        static char[] separatorArray = { ' ' };
         static void Main(string[] args)
         {
             if (args.Length > 0)
             {
                 var filename = args[0];
-                string[] lines = System.IO.File.ReadAllLines(filename);
-                int currentLevel = 0;
-                int levelsOpen = 0;
-                int tabNum = 0;
-                string s = "";
-                string type = "";
-                string output = "BehaviorTree tree = new BehaviorTreeBuilder(\"\")";
-                string[] parts;
-                foreach (var line in lines)
+                if (System.IO.File.Exists(filename))
                 {
-                    tabNum = line.Count(ch => ch == '\t');
-                    if (0 < tabNum && tabNum <= currentLevel)
+                    string[] lines = System.IO.File.ReadAllLines(filename);
+                    int currentLevel = 0;
+                    int levelsOpen = 0;
+                    int tabNum = 0;
+                    string s = "";
+                    string type = "";
+                    string output = "BehaviorTree tree = new BehaviorTreeBuilder(\"\")";
+                    string[] parts;
+                    foreach (var line in lines)
+                    {
+                        tabNum = line.Count(ch => ch == '\t');
+                        if (0 < tabNum && tabNum <= currentLevel)
+                        {
+                            output += "\n.End()";
+                            levelsOpen--;
+                        }
+                        s = line.Substring(tabNum);
+                        parts = s.Split(separatorArray, 2);
+                        type = parts[0];
+                        if (type == "#")
+                        {
+                            output += "\n.Do(\"" + parts[1] + "\", () => {})";
+                        }
+                        else if (type == "&")
+                        {
+                            output += "\n.Sequence(\"" + parts[1] + "\")";
+                            currentLevel = tabNum;
+                            levelsOpen++;
+                        }
+                        else if (type == "|")
+                        {
+                            output += "\n.Selector(\"" + parts[1] + "\")";
+                            currentLevel = tabNum;
+                            levelsOpen++;
+                        }
+                        else if (type == "?")
+                        {
+                            output += "\n.If(\"" + parts[1] + "\", () => {})";
+                        }
+                        else if (type == "!")
+                        {
+                            output += "\n.Not(\"" + parts[1] + "\")";
+                        }
+                        else if (Regex.IsMatch(type, @"\d"))
+                        {
+                            int result;
+                            if (int.TryParse(type, out result))
+                            {
+                                output += "\n.Repeat(\"" + parts[1] + (result > 0 ? "\", " + result.ToString() : "") + ")";
+                            }
+                        }
+                        else if (type == "*")
+                        {
+                            output += "\n.RepeatUntilFail(\"" + parts[1] + "\")";
+                        }
+                        else if (type == "^")
+                        {
+                            output += "\n.Ignore(\"" + parts[1] + "\")";
+                        }
+                        else if (type == "\"")
+                        {
+                            output += "\n.Wait(\"" + parts[1] + "\", 0)";
+                        }
+                        else
+                        {
+                            // Do nothing
+                        }
+                    }
+                    while (levelsOpen > 0)
                     {
                         output += "\n.End()";
                         levelsOpen--;
                     }
-                    s = line.Substring(tabNum);
-                    parts = s.Split(separatorArray, 2);
-                    type = parts[0];
-                    if (type == "#")
-                    {
-                        output += "\n.Do(\"" + parts[1] + "\", () => {})";
-                    }
-                    else if (type == "&")
-                    {
-                        output += "\n.Sequence(\"" + parts[1] + "\")";
-                        currentLevel = tabNum;
-                        levelsOpen++;
-                    }
-                    else if (type == "|")
-                    {
-                        output += "\n.Selector(\"" + parts[1] + "\")";
-                        currentLevel = tabNum;
-                        levelsOpen++;
-                    }
-                    else if (type == "?")
-                    {
-                        output += "\n.If(\"" + parts[1] + "\", () => {})";
-                    }
-                    else if (type == "!")
-                    {
-                        output += "\n.Not(\"" + parts[1] + "\")";
-                    }
-                    else if (Regex.IsMatch(type, @"\d"))
-                    {
-                        int result;
-                        if (int.TryParse(type, out result))
-                        {
-                            output += "\n.Repeat(\"" + parts[1] + (result > 0 ? "\", " + result.ToString() : "") + ")";
-                        }
-                    }
-                    else if (type == "*")
-                    {
-                        output += "\n.RepeatUntilFail(\"" + parts[1] + "\")";
-                    }
-                    else if (type == "^")
-                    {
-                        output += "\n.Ignore(\"" + parts[1] + "\")";
-                    }
-                    else if (type == "\"")
-                    {
-                        output += "\n.Wait(\"" + parts[1] + "\", 0)";
-                    }
-                    else
-                    {
-                        // Do nothing
-                    }
-                }
-                while (levelsOpen > 0)
-                {
-                    output += "\n.End()";
-                    levelsOpen--;
-                }
-                output += ";";
+                    output += ";";
 
-                System.IO.File.AppendAllText("output.cs", output);
+                    System.IO.File.AppendAllText("output.txt", output);
+                }
+                else
+                {
+                    Console.WriteLine("Unable to find file");
+                }
             }
         }
     }
---
+++ C++/BTMLParserCPP/Source.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> separateStringBy(const string& s, const char& d)
+{
+	vector<string> parts;
+	int i = s.find_first_of(d);
+	parts.push_back(s.substr(0, i));
+	parts.push_back(s.substr(i + 1));
+	return parts;
+}
+
+string trimString(const string& s)
+{
+	string str = s;
+	const string whitespace = " \t";
+	const auto begin = str.find_first_not_of(whitespace);
+	if (begin == std::string::npos)
+		return "";
+
+	const auto end = str.find_last_not_of(whitespace);
+	const auto range = end - begin + 1;
+	return str.substr(begin, range);
+}
+
+bool isNumber(const std::string& s)
+{
+	return !s.empty() && s.find_first_not_of("0123456789") == std::string::npos;
+}
+
+int countTabs(string s)
+{
+	int num = 0;
+	while (s[0] == '\t')
+	{
+		s = s.substr(1);
+		num++;
+	}
+	return num;
+}
+
+string getLambda(string type)
+{
+	string returnValue = type == "action" ? "EStatus::ERROR" : "false";
+	return "[]() { return " + returnValue + "; }";
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc > 0)
+	{
+		char* filename = argv[1];
+		ifstream ifile(filename);
+		if (ifile)
+		{
+			const char separator = ' ';
+			string line = "";
+
+			int currentLevel = 0;
+			int levelsOpen = 0;
+			int tabNum = 0;
+			string type = "";
+			vector<string> parts;
+
+			string output = "BehaviorTree tree = BehaviorTreeBuilder(\"\")";
+
+			while (getline(ifile, line))
+			{
+				tabNum = countTabs(line);
+				if (0 < tabNum && tabNum <= currentLevel)
+				{
+					output += "\n.End()";
+					levelsOpen--;
+				}
+
+				istringstream iss(trimString(line));
+				parts = separateStringBy(iss.str(), separator);
+				type = parts[0];
+
+				if (type == "#")
+				{
+					output += "\n.Do(\"" + parts[1] + "\", " + getLambda("action") + ")";
+				}
+				else if (type == "&")
+				{
+					output += "\n.Sequence(\"" + parts[1] + "\")";
+					currentLevel = tabNum;
+					levelsOpen++;
+				}
+				else if (type == "|")
+				{
+					output += "\n.Selector(\"" + parts[1] + "\")";
+					currentLevel = tabNum;
+					levelsOpen++;
+				}
+				else if (type == "?")
+				{
+					output += "\n.If(\"" + parts[1] + "\"," + getLambda("condition") + ")";
+				}
+				else if (type == "!")
+				{
+					output += "\n.Not(\"" + parts[1] + "\")";
+				}
+				else if (isNumber(type))
+				{
+					int result = stoi(type);
+					output += "\n.Repeat(\"" + parts[1] + (result > 0 ? "\", " + type : "") + ")";
+				}
+				else if (type == "*")
+				{
+					output += "\n.RepeatUntilFail(\"" + parts[1] + "\")";
+				}
+				else if (type == "^")
+				{
+					output += "\n.Ignore(\"" + parts[1] + "\")";
+				}
+				else if (type == "\"")
+				{
+					output += "\n.Wait(\"" + parts[1] + "\", 0)";
+				}
+				else
+				{
+					// Do nothing
+				}
+			}
+			ifile.close();
+			while (levelsOpen > 0)
+			{
+				output += "\n.End()";
+				levelsOpen--;
+			}
+			output += ";";
+
+			std::remove("output.txt");
+			ofstream ofile("output.txt");
+			ofile.clear();
+			ofile << output;
+			ofile.close();
+		}
+	}
+	else
+	{
+		cout << "Unable to find file" << endl;
+	}
+	return 0;
+}
